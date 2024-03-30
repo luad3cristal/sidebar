495939e14c7736c1bb211f625a992dacbbd3c02c
Luana <luanalima.2006@hotmail.com>
2024-03-30 00:49:43 -0300

    trial: hiding content with javascript

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const menu = document.querySelector("#menu")
 const sidebar = document.querySelector(".sidebar")
-const hidden = document.querySelector(".hidden")
 
 menu.onfocus = function () {
   sidebar.classList.toggle("active")
-  hidden.classList.toggle("hidden")
 }
